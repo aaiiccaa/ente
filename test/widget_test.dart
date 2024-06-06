44483ca020ca54bbb41a0a9db0384ce1ebe1804b
aaiiccaa <aishafarizka.a@gmail.com>
2024-06-06 20:23:38 +0700

    added dashboard page with filter food type & location

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ente/main.dart';
+import 'package:ente/dashboard.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
