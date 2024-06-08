733c5a4d5f17483b65cf01d510be97768e1aae87
Devi Zahra Aulia <152952973+depjarr@users.noreply.github.com>
2024-06-08 22:04:50 +0700

    Welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,61 @@
 import 'package:flutter/material.dart';
-import 'dashboard.dart';
+import 'dashboard.dart'; // Pastikan ini mengarah ke lokasi file dashboard yang benar
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WelcomeApp());
 }
+
+class WelcomeApp extends StatelessWidget {
+  const WelcomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to ENTe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Inter',
+      ),
+      home: const WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  void initState() {
+    super.initState();
+    // Menunda navigasi ke dashboard selama 3 detik
+    Future.delayed(Duration(seconds: 3), () {
+      if (mounted) { // Pastikan widget masih dalam tree
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const MyHomePage(title: 'ENTe')));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to ENTe',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black, // Menambahkan warna untuk memastikan kontras
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 
