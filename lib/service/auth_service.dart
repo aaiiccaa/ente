cd6bdba354fd43e9473597590db79d73b084f95e
Tsania Millatina Aghnia Fariha <122031277+tsaniafarihaa@users.noreply.github.com>
2024-06-16 00:38:06 +0700

    Add files via upload

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  final String baseUrl = "http://ente.my.id/api";
+
+  Future<void> saveToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_token', token);
+  }
+
+  Future<Map<String, dynamic>> register(
+      String name, String email, String password) async {
+    final url = Uri.parse('$baseUrl/register');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'email': email,
+        'password': password,
+        'password_confirmation': password, // Assuming password confirmation
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      final data = jsonDecode(response.body);
+      await saveToken(data['token']);
+      return data;
+    } else {
+      throw Exception('Failed to register');
+    }
+  }
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final url = Uri.parse('$baseUrl/login');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      await saveToken(data['token']);
+      return data;
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+}
